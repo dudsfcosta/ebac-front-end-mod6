e4f65f383c00034c2e285639ed12d1640b2aae6b
Eduarda Ferreira Costa <77804572+dudsfcosta@users.noreply.github.com>
2025-12-06 10:49:05 -0300

    ajustes para entrega da atividade.

--- Atividade/script.js
+++ Atividade/script.js
@@ -1,7 +1,9 @@
 // Gera um número aleatório entre 1 e 100
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 console.log(randomNumber)
-let chances = 0;
+
+// Variáveis de controle
+let chances = 10;
 console.log(chances)
 let flag = false;
 
@@ -11,31 +13,38 @@ function chutar() {
     const tentativa = parseInt(document.getElementById('number').value);
     // Resultado
     const resultado = document.getElementById('resultado');
+    
+    if (tentativa < 1 || tentativa > 100) {
+        
+        resultado.textContent = 'Forneça um número entre 1 e 100.';
+        return;
+    }
 
-    if (chances < 10) {
+    while(chances >= 0) {
         
         // Compara se o resultado é igual, maior ou menor
         if (tentativa === randomNumber) {
 
-            resultado.textContent = 'Parabéns! Você acertou o número!';
+            resultado.textContent = 'Você acertou! Recarregue a página para recomeçar.';
             flag = true;
-            chances = 9;
+            chances = -1;
         } else if (tentativa < randomNumber) {
 
-            resultado.textContent = `O seu número é menor. Tente novamente. \n Tentativas restantes: ${10-chances}`;
+            resultado.textContent = `O número secreto é maior. Tente novamente. \n Tentativas restantes: ${chances-1}`;
+            chances--;
         } else {
 
-            resultado.textContent = `O seu número é maior. Tente novamente. \n Tentativas restantes: ${10-chances}`;
+            resultado.textContent = `O número secreto é menor. Tente novamente. \n Tentativas restantes: ${chances-1}`;
+            chances--;
         }
-        chances++;
-        console.log(chances);
-    } else {
-        
-        if (flag) {
-            
-            resultado.textContent = 'Você já acertou o número. Recarregue a página para recomeçar.'
-            return;
-        }
+        document.getElementById('number').value = '';
+        return;
+    }
+    if (flag) {
+
+        resultado.textContent = 'Você já acertou o número. Recarregue a página para recomeçar.'
+    } else if (!flag && chances === 0) {
+
         resultado.textContent = 'Você não tem mais tentativas. Recarregue a página para recomeçar.'
     }
 }
